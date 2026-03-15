7ec0dbfb58eda19049a8f73cbf5d34fef0b90e14
BetotPetot <irhamfarel37@gmail.com>
2026-03-15 16:07:44 +0700

    second commit

---
+++ TP_MODUL4_103022400105/KodePos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL4_103022400105
+{
+    internal class KodePos
+    {
+        string[] kelurahan = {
+            "Batununggal", "Kujangsari", "Mengger",
+            "Wates", "Cijaura", "Jatisari",
+            "Margasari", "Sekejati", "Kebonwaru", "Maleer"
+        };
+
+        int[] kodePos = {
+            40266, 40287, 40267,
+            40256, 40287, 40286,
+            40286, 40286, 40272, 40274
+        };
+
+        public int getKodePos(String kel) {
+            for (int i = 0; i < kelurahan.Length; i++) {
+                if (kelurahan[i] == kel) { 
+                    return kodePos[i];
+                }
+            } 
+            return -1;
+        }
+
+    }
+}
--- TP_MODUL4_103022400105/Program.cs
+++ TP_MODUL4_103022400105/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL4_103022400105;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+
+        Console.Write("Masukkan Kelurahan: ");
+        string kel = Console.ReadLine();
+
+        int kode = kp.getKodePos(kel);
+
+        Console.WriteLine("Kode Pos: " + kode);
+    }
+}
